d444910c4f82c2c2b8d70ae1e3567bdfc174b3c1
Sean Deering <sdeering@ucsd.edu>
2024-10-29 01:19:00 -0700

    updated projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,23 +1,23 @@
 <script>
   let root = document.documentElement;
-  let colorScheme = localStorage.colorScheme ?? 'light dark';
+  let colorScheme = localStorage.colorScheme ?? 'dark'; // Default to dark mode
   root.style.setProperty('color-scheme', colorScheme);
 
   import projects from '$lib/projects.json';
   import Project from '$lib/Project.svelte';
 </script>
-  
-  <svelte:head>
-    <title>Projects</title>
-  </svelte:head>
-  
-  <h1>Projects</h1>
 
-  <div class="projects">
-      {#each projects as p}
-        <Project data={p} />  
-      {/each}
-  </div>
+<svelte:head>
+  <title>Projects</title>
+</svelte:head>
 
-
-    
+<h1>Projects</h1>
+<div class="projects">
+  {#if projects.length > 0}
+    {#each projects as p}
+      <Project data={p} />  
+    {/each}
+  {:else}
+    <p>No projects found.</p>
+  {/if}
+</div>
