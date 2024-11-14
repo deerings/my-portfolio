937e0ebab7fca5893042423be68c3a4b4a60e25b
Sean Deering <sdeering@ucsd.edu>
2024-11-13 20:14:45 -0800

    attempted fix #1

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,31 @@
+// import adapter from '@sveltejs/adapter-static';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter({
+// 			fallback: '404.html'
+// 		}),
+// 		paths: {
+// 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+// 		},
+// 	},
+// };
+
+// export default config;
+
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
-	},
+  kit: {
+    adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '/your-repo-name'
+    }
+  }
 };
 
 export default config;
