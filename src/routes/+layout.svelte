2f733986f8efbba80a28a2ddff0e3d019adc7c7b
Sean Deering <sdeering@ucsd.edu>
2025-01-04 00:45:28 -0800

    fixes 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,12 +47,13 @@
 </label>
 
 <nav>
-    <a href="/" class={$page.url.pathname === '/' ? 'active' : ''}>Home</a>
-    <a href="/projects" class={$page.url.pathname === '/projects' ? 'active' : ''}>Projects</a>
-    <a href="/cv" class={$page.url.pathname === '/cv' ? 'active' : ''}>CV</a>
-    <a href="/contact" class={$page.url.pathname === '/contact' ? 'active' : ''}>Contact</a>
+    <a href="/my-portfolio/" class={$page.url.pathname === '/my-portfolio/' ? 'active' : ''}>Home</a>
+    <a href="/my-portfolio/projects" class={$page.url.pathname === '/my-portfolio/projects' ? 'active' : ''}>Projects</a>
+    <a href="/my-portfolio/cv" class={$page.url.pathname === '/my-portfolio/cv' ? 'active' : ''}>CV</a>
+    <a href="/my-portfolio/contact" class={$page.url.pathname === '/my-portfolio/contact' ? 'active' : ''}>Contact</a>
     <a href="https://www.github.com/deerings" target="_blank" rel="noopener noreferrer">Github</a>
-  </nav>
+</nav>
+
 
 <style>
   .active {
